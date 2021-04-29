57fd148ae017355610e4119a98f137688901bc75
volpito <mpj.rigot@gmail.com>
2021-04-29 22:16:30 +0200

    Gaming list & search OK

--- src/js/PageList.js
+++ src/js/PageList.js
@@ -6,7 +6,7 @@ const PageList = (argument) => {
     const fetchList = (url, argument) => {
       let finalURL = url;
       if (argument) {
-        finalURL = `https://api.rawg.io/api/games?/page_size=9&search=${argument}&key=4213f27351ba43b8a9f008de6de62a91`;
+        finalURL = `https://api.rawg.io/api/games?page_size=9&search=${argument}&key=4213f27351ba43b8a9f008de6de62a91`;
         console.log(finalURL);
       }
 
@@ -18,7 +18,7 @@ const PageList = (argument) => {
                   <div class="card" style="width: 18rem;">
                     <img src="${article.background_image}" class="card-img-top" alt=".poster..">
                     <div class="card-body">
-                      <h5 class="card-title">${article.name}</h5>
+                      <h5 class="card-title">${article.name} </h5>
                       <p class="card-text">Release date : ${article.released}</p>
                       <p class="card-text">Platforms : ${article.platforms.map(a => " " + a.platform.name)}</p>
                       <a href="#pagedetail/${article.id}" class="btn btn-dark">More</a>
@@ -29,7 +29,7 @@ const PageList = (argument) => {
           document.querySelector(".page-list .articles").innerHTML = articles;
         });
     };
-    fetchList(`https://api.rawg.io/api/games?key=4213f27351ba43b8a9f008de6de62a91`, cleanedArgument);
+    fetchList(`https://api.rawg.io/api/games?page_size=9&key=4213f27351ba43b8a9f008de6de62a91`, cleanedArgument);
   };
 
   const render = () => {
@@ -50,7 +50,7 @@ const PageList = (argument) => {
       let arg = searchBar.value;
       console.log(arg);
       pageContent.innerHTML = `
-      <p> MES COUILLES </P>
+      <h1> Votre recherche : </h1>
       `;
       PageList(arg);
     })};
